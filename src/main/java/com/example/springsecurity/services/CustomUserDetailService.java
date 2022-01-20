9c339467078eb850589963997498f15d44576e4a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-01-20 09:26:56 +0100

    user details service updated

--- src/main/java/com/example/springsecurity/services/CustomUserDetailService.java
+++ src/main/java/com/example/springsecurity/services/CustomUserDetailService.java
@@ -1,24 +1,50 @@
 package com.example.springsecurity.services;
 
-import org.springframework.security.core.userdetails.User;
+import com.example.springsecurity.data.models.User;
+import com.example.springsecurity.data.repositories.UserRepository;
+import com.example.springsecurity.dtos.request.UserRequest;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 
 @Service
 public class CustomUserDetailService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Lazy
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public UserRequest register (UserRequest userRequest){
+
+        User user = new User();
+        BeanUtils.copyProperties(userRequest, user);
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+        user = userRepository.save(user);
+        BeanUtils.copyProperties(user, userRequest);
+        return  userRequest;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        if(username.equals("goodnews")){
-            return new User("goodnews", "uche", new ArrayList<>());
-        }else {
+        User user = userRepository.findByUsername(username);
+
+        if( user == null){
             throw new UsernameNotFoundException("User does not exist");
         }
+
+        UserRequest userRequest = new UserRequest();
+        BeanUtils.copyProperties(user, userRequest);
+
+        return userRequest;
     }
 }
--- src/main/java/com/example/springsecurity/services/RoleService.java
+++ src/main/java/com/example/springsecurity/services/RoleService.java
@@ -1,2 +1,17 @@
-package com.example.springsecurity.services;public interface RoleService {
+package com.example.springsecurity.services;
+
+import com.example.springsecurity.data.models.Role;
+import com.example.springsecurity.dtos.request.RoleRequest;
+
+import java.util.List;
+
+public interface RoleService {
+
+     RoleRequest createRole(RoleRequest roleRequest);
+
+     List<RoleRequest> getAllRoles();
+
+     RoleRequest getRoleById(Long roleId);
+
+    void deleteRoleById(Long id);
 }
